b1a5af4398c74bf2227adcf3746752fbbd74a695
j3nnn1 <j3nnn1@Diotima.localdomain>
2011-04-07 20:27:36 -0430

    compartiendo con gnome3

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -3,15 +3,19 @@
 . $GLOBALAUTOSTART
 
 # Programs to launch at startup
+setxkbmap latam&
+gconftool-2 --set /apps/gnome_settings_daemon/plugins/background/active --type bool False
 nitrogen --restore&
 xcompmgr -c -t-5 -l-5 -r4.2 -o.55 &
 
 
 # Programs that will run after Openbox has started
 (sleep 2 && pypanel) &
-nautilus &
+#nautilus &
+nitrogen --restore & 
 volwheel &
-wicd-client &
+#wicd-client &
 conky&
+skype&
 #blueman-manager&
 #guake &
