62dc2eb6e92f73751a92d1c5665b56f9b1a5cb6a
j3nnn1 <jcmm986@gmail.com>
2013-08-31 12:22:31 -0300

    updating another tool

--- archlinux/update_mirrorlist.sh
+++ archlinux/update_mirrorlist.sh
@@ -3,3 +3,5 @@
 cp /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist.backup
 sed '/^#\S/ s|#||' -i /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+
+#reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
