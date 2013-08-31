73447e7a60ccbcbc90225acd1e870cde377aebc3
j3nnn1 <jcmm986@gmail.com>
2013-08-31 12:17:14 -0300

    update mirrorlist by download speed

---
+++ archlinux/update_mirrorlist.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist.backup
+sed '/^#\S/ s|#||' -i /etc/pacman.d/mirrorlist.backup
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
