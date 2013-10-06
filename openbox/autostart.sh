6cce42c972488b7410380a0b96660a8aebcb9d49
j3nnn1 <jcmm986@gmail.com>
2013-10-06 11:00:29 -0300

    updating autostart new seventh

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,20 +1,22 @@
-
 # Run the system-wide support stuff
-. $GLOBALAUTOSTART
+	$GLOBALAUTOSTART
 
 # Programs to launch at startup
 setxkbmap latam&
 gconftool-2 --set /apps/gnome_settings_daemon/plugins/background/active --type bool False
-nitrogen --restore&
-xcompmgr -c -t-5 -l-5 -r4.2 -o.55 &
 
 
-#blueman-manager&
 # Programs that will run after Openbox has started
+#(sleep 2 && pypanel) &
 (sleep 2 && fbpanel ) &
-nitrogen --restore & 
+#xcompmgr -c -t-5 -l-5 -r4.2 -o.55 &
+nitrogen --restore &
 conky&
 skype&
-pytyle&
 tint2&
+pytyle&
+#wicd-client &
+#blueman-applet&
 /usr/lib/notification-daemon-1.0/notification-daemon&
+
+#/usr/lib/notification-daemon-1.0/notification-daemon
